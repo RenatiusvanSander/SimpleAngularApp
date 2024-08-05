31f228eea8e5081460590b2da1d781ee52a4fbc5
Remy Meier <remad@web.de>
2024-08-05 20:45:29 +0200

    typescript: how to get a reference of a private object inside a component

--- src/app/page1/page1.component.spec.ts
+++ src/app/page1/page1.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Page1Component } from './page1.component';
+import { DataService } from '../data.service';
+import { Book } from '../model/Book';
 
 describe('Page1Component', () => {
   let component: Page1Component;
@@ -19,5 +21,14 @@ describe('Page1Component', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Number of books written by Matt is incremented correctly', () => {
+    const startValue = component.numberOfBooksWrittenByMatt;
+    const book = new Book();
+    book.author = 'matt';
+    const dataService = fixture.debugElement.injector.get(DataService);
+    dataService.addBook(book);
+    expect(component.numberOfBooksWrittenByMatt).toEqual(startValue + 1);
   });
 });
